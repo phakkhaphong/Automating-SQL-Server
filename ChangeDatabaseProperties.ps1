16c23f74fded4697eaa5e19be41f0ec204ffe5e6
phakkhaphong <phakkhaphong@msn.com>
2023-09-21 17:20:36 -0700

    Create ChangeDatabaseProperties.ps1

---
+++ ChangeDatabaseProperties.ps1
@@ -0,0 +1,28 @@
+﻿#1 Import SQLPS
+
+Clear-Host;
+Import-Module SQLPS;
+
+#2 Navigate to the Databases collection in the default instance 
+
+Set-Location SQLSERVER:
+Set-Location \SQL\localhost\DEFAULT\Databases;
+
+#3 Get the database that we want to work with
+
+$databaseName = "AdventureWorks";
+$database = Get-Item -Path $databaseName;
+    
+#4 Change some of the database properties
+$database.CompatibilityLevel = [Microsoft.SqlServer.Management.Smo.CompatibilityLevel]::Version150;
+$database.Alter();
+Write-Host "Changed database properties";
+Write-Host $database.CompatibilityLevel
+
+#5 Change some of the database options
+$database.DatabaseOptions.AnsiNullsEnabled = $false;
+$database.DatabaseOptions.AutoShrink = $true;
+$database.DatabaseOptions.ReadOnly = $true;
+$database.DatabaseOptions.RecoveryModel = [Microsoft.SqlServer.Management.Smo.RecoveryModel]::Full;
+$database.Alter();
+Write-Host "Changed database options";
